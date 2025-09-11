01d8d9a9dd78ffad0d9f0f64e9556a0448577744
vrvrakk <vrvrakk700@gmail.com>
2025-09-11 11:21:49 +0200

    section 7 algebra: gauss

--- section_7_Algebra2.py
+++ section_7_Algebra2.py
@@ -1,6 +1,7 @@
 # ALGEBRA 2
 import numpy as np
 import sympy as sym
+import scipy as sp
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -101,10 +102,10 @@ quadeqN= (-b - np.sqrt(b**2 - 4*a*c)) / (2 * a)
 import scipy as sp
 def quadeq(a, b, c):
     # initialize
-    out = np.zeros(2)
+    out = sp.zeros(2)
     out[0] = (-b + np.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
     out[1] = (-b - np.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-    return  out
+    return out
 
 out = quadeq(a, b, c)
 
@@ -194,6 +195,15 @@ assert eq1 == eq2
 # and if z = a - bi, then the z* = a + bi -> essentially flip the imaginary number
 # z = a bi
 # then apparently... z = a - bi ? whyyyyyyy I think it's wrong
+# ANSWER:
+'Hi Varvara. The notation with complex numbers can be tricky. "a -bi" is one number, ' \
+'not the subtraction of two numbers. ' \
+'If you think about a complex number as a coordinate on a 2D plane, ' \
+'then [a,-bi] are the coordinates of that number.'
+'So, the complex conjugate really does involve just flipping the sign of the imaginary part, not subtracting or multiplying.'
+'To multiply two complex numbers, you have to expand out the multiplication:'
+'[a ib]*[a -ib] = [a*a - a*ib +ib*a - ib*(-ib)] = [a^2 - (-i)^2*b*2] = [a^2 + b^2]'
+
 
 z1 = complex(4, 5)
 z2 = complex(6, -2)
@@ -238,3 +248,165 @@ eq1 = sym.simplify((z - 1) * (z*w + w) / w*z - w)
 eq2 = sym.simplify((w-1) * (1 - w) / -z*w**2 - w**2 + 2*w*z + 2*w - z - 1)
 
 res = eq1 * eq2
+
+# Graphing complex numbers
+# imaginary axis: im (y)
+# real axis : re (x)
+# z = 2 3i -> (2, 3) -> the plane is called the COMPLEX PLANE
+
+# example:
+z = complex(2, 3)
+# we need to extract the real and imag part from the complex number before plotting
+plt.plot(np.real(z),np.imag(z),  'ro') # adding a line connecting to the origin of the plane
+plt.plot([0, np.real(z)], [0, np.imag(z)], 'r')
+plt.xlabel('real')
+plt.ylabel('imaginary')
+plt.grid()
+plt.axis([-4, 4, -4, 4])
+
+# exercise: plotting complex numbers
+z1 = complex(-3, 1)
+z2 = complex(-1, 1)
+# case 1: plot z1, z2 and z1+z2
+# case 2: same, but z1 * z2
+eq1 = z1 + z2
+eq2 = z1 * z2
+
+fig, axis = plt.subplots(1, 2)
+axis[0].plot(np.real(z1), np.imag(z1))
+axis[0].plot([0, np.real(z1)], [0, np.imag(z1)], 'b')
+axis[0].plot(np.real(z2), np.imag(z2))
+axis[0].plot([0, np.real(z2)], [0, np.imag(z2)], 'orange')
+axis[0].plot(np.real(eq1), np.imag(eq1))
+axis[0].plot([0, np.real(eq1)], [0, np.imag(eq1)], 'g')
+axis[0].axis('square')
+axis[0].grid()
+axis[0].axis([-5, 5, -5, 5])
+
+
+axis[1].plot(np.real(z1), np.imag(z1))
+axis[1].plot([0, np.real(z1)], [0, np.imag(z1)], 'b')
+axis[1].plot(np.real(z2), np.imag(z2))
+axis[1].plot([0, np.real(z2)], [0, np.imag(z2)], 'orange')
+axis[1].plot(np.real(eq2), np.imag(eq2))
+axis[1].plot([0, np.real(eq2)], [0, np.imag(eq2)], 'g')
+axis[1].axis('square')
+axis[1].grid()
+axis[1].axis([-5, 5, -5, 5])
+# success, took forever
+
+# generate heatmaps with real and imaginary parts this time. keeping therefore complex numbers
+a = 1
+b = np.arange(-5, 6)
+c = np.arange(-2, 11)
+# we need to specify here that the matrix needs to be able to handle complex numbers
+M = np.zeros((len(b), len(c)), dtype='complex')
+for bi, b_val in enumerate(b):
+    for ci, c_val in enumerate(c):
+        out = quadeq(a, b_val, c_val)
+        M[bi, ci] = out[0]
+
+plt.subplot(1, 3, 1)  # 1 row, 3 columns, axis 1
+plt.imshow(np.real(M), extent=[c[0], c[-1], b[0], b[-1]],cmap='viridis')
+# changed M to np.real(M)
+plt.axis('off')
+plt.title('real part')
+
+plt.subplot(1, 3, 2)
+plt.imshow(np.imag(M), extent=[c[0], c[-1],b[0], b[-1]], cmap='viridis')
+plt.axis('off')
+plt.title('imag part')
+
+plt.subplot(1, 3, 3)
+plt.imshow(np.absolute(M), extent=[c[0], c[-1],b[0], b[-1]], cmap='viridis')
+plt.axis('off')
+plt.title('magnitude')
+plt.show()
+
+# The unit circle, and how to create it
+# a circle, centered a the origin
+# radius = 1
+x = np.linspace(0, 2*np.pi, 360)
+
+plt.plot(np.cos(x), np.sin(x), 'k')
+plt.plot([-1.1, 1.1], [0, 0], '--', color=[0.5, 0.5, 0.5]) # for a gray line
+plt.plot([0,0], [-1.1, 1.1], '--', color=[0.5, 0.5, 0.5]) # for a gray line
+
+angle = np.pi/4
+plt.plot([0, np.cos(angle)],[0, np.sin(angle)])
+
+plt.axis('square')
+plt.axis([-1.1, 1.1, -1.1, 1.1])
+plt.xlabel('cos(x)')
+plt.ylabel('sin(x)')
+plt.show()
+
+# exercise: produce a circle with radius lines starting from origin and ending at k = e**x
+x = np.linspace(-3, 1.83, 50)
+
+angles = [np.e**x_value for x_value in x]
+for angle in angles:
+    plt.plot([0, np.cos(angle)], [0, np.sin(angle)])
+plt.axis('square')
+plt.axis('off') # how to remove frame with ticks!
+plt.show()
+
+# logarithmic and exponential plots
+# y = e**x
+# y = log(x)
+x = np.linspace(-3, 3, 99)
+
+# e of x
+plt.subplot(1, 2, 1)
+plt.plot(x, np.exp(x))
+plt.xlabel('x')
+plt.ylabel('$e^x$')
+plt.show()
+
+# log of x
+plt.subplot(1, 2, 2)
+plt.plot(x, np.log(x))
+plt.xlabel('x')
+plt.ylabel('log(x)')
+plt.show()
+
+# exercise on log and e:
+# plot y1 and y2
+x = np.linspace(.0001, 10, 10)
+y1 = [np.log(np.exp(x_value)) for x_value in x]
+y2 = [np.exp(np.log(x_value)) for x_value in x]
+
+plt.plot(x, y1, label='$log(e^x)$')
+plt.plot(x, y2, 'o', label='$e^log(x)$') # it automatically colors it orange, if I leave the previous one unspecified
+
+plt.axis('square')
+plt.axis([0, 10, 0, 10])
+plt.legend()
+plt.show()
+
+# GAUSSIAN CURVES: find a specific point on the gaussian
+# y = np.exp(-4ln(2)t^2) / h^2
+# t usually for time
+# full width at half maximum:
+# between 1 and 0 of y (max and min), the half point -> 0.5
+# find the two points of the curve at 0.5 -> connect horizontally and vertically
+# distance between horizontal line of the two points connected is the FWHM and its distance == 1
+# so h = FWHM
+t = np.linspace(-3, 3, 1001) # if we use even number of steps, we do not get step 0 exactly.
+h = 0.9  # 1 is max
+gaussian = np.exp(-4*np.log(2)*t**2 / h**2)
+
+# plot:
+plt.plot(t, gaussian)
+plt.xlabel('t')
+plt.ylabel('y')
+plt.title('Gaussian with FWHM=' + str(h))
+
+plt.show()
+
+# example 2:
+yval2find = .5
+tvals = h * np.sqrt(np.log(yval2find) / (-4*np.log(2))) # returns the non-negative square root (the principle square root)
+print(tvals, -tvals)
+
+# gauss exercise:
